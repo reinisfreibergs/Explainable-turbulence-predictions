6bdbc159a11d01ac6e2a78400d1f1705d6b0822a
Reinis Freibergs <reinisfreibergs@gmail.com>
2024-02-26 19:05:26 +0100

    Update and modified model check

--- FCN-turbulence-predictions-from-wall-quantities-master/main_v2.py
+++ FCN-turbulence-predictions-from-wall-quantities-master/main_v2.py
@@ -5,6 +5,16 @@ import numpy as np
 import time
 import tensorflow as tf
 import sys
+import matplotlib.pyplot as plt
+import platform
+if platform.system() == 'Windows':
+    import matplotlib
+    matplotlib.use("qt5agg")
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
+import shap
+from tqdm import tqdm
 
 sys.path.insert(0, '../conf')
 sys.path.insert(0, '../models')
@@ -12,11 +22,12 @@ sys.path.insert(0, '../models')
 # Training utils
 from src.training_utils import load_trained_model
 from src.tfrecord_utils import get_dataset
-from conf.config_sample import WallRecon
+# from conf.config_sample import WallRecon
 
 # %% Configuration import
 import config
 
+prb_def = 'WallReconfluct'
 # prb_def = os.environ.get('MODEL_CNN', None)
 #
 # if not prb_def:
@@ -31,7 +42,7 @@ import config
 # else:
 #     raise ValueError('"MODEL_CNN" enviroment variable must be defined either as "WallRecon" or "OuterRecon"')
 
-app = WallRecon
+app = config.WallRecon
 
 os.environ["CUDA_VISIBLE_DEVICES"] = str(app.WHICH_GPU_TEST);
 # os.environ["CUDA_VISIBLE_DEVICES"]="";
@@ -49,7 +60,7 @@ app.TRANSFER_LEARNING = False
 physical_devices = tf.config.list_physical_devices('GPU')
 availale_GPUs = len(physical_devices)
 print('Using TensorFlow version:', tf.__version__, ', GPU:', availale_GPUs)
-print(tf.keras.__version__)
+# print(tf.keras.__version__)
 
 if physical_devices:
     try:
@@ -92,7 +103,7 @@ if app.INIT == 'model':
 print(f'Prediction of fluctuation only: {app.FLUCTUATIONS_PRED}')
 print(f'y- and z-output scaling with the ratio of RMS values : {app.SCALE_OUTPUT}')
 print(f'Normalized input: {app.NORMALIZE_INPUT}')
-print(f'File for input statistics: {app.AVG_INPUTS_FILE}')
+# print(f'File for input statistics: {app.AVG_INPUTS_FILE}')
 print('')
 print('# ====================================================================')
 # %% Testing
@@ -142,29 +153,52 @@ CNN_model = load_trained_model(model_config)
 Y_pred = np.ndarray((n_samples_tot, app.N_VARS_OUT,
                      model_config['nx_'], model_config['nz_']), dtype='float')
 
-if app.N_VARS_OUT == 2:
-    (Y_pred[:, 0, np.newaxis], Y_pred[:, 1, np.newaxis]) = CNN_model.predict(
-        X_test, batch_size=model_config['batch_size'])
+
+
+avg_inputs = np.broadcast_to(np.mean(np.mean(X_test, axis=-1, keepdims=True),
+                                     axis=-2, keepdims=True),
+                             X_test.shape)
+
 if app.N_VARS_OUT == 3:
+    # reduce batch size for smaller gpu
     (Y_pred[:, 0, np.newaxis], Y_pred[:, 1, np.newaxis], Y_pred[:, 2, np.newaxis]) = \
-        CNN_model.predict(X_test, batch_size=model_config['batch_size'])
+        CNN_model.predict(X_test, batch_size=2)
 
-print(type(Y_pred))
-print(np.shape(Y_pred))
+comparative_idx = 2
+mse_orig = np.mean((Y_pred[:, comparative_idx, :, :] - Y_test[:, comparative_idx, :, :])**2)
 
-# Revert back to the flow field
-if app.SCALE_OUTPUT == True:
-    u_rms = model_config['rms'][0] \
-        [model_config['ypos_Ret'][str(app.TARGET_YP)]]
+for i in tqdm(range(len(X_test))):
+    output_mse = tf.reduce_mean(tf.reduce_mean(tf.math.square(tf.math.subtract(CNN_model.output[comparative_idx][:, 0, :, :], Y_test[i][comparative_idx][None, :])), axis=-1), axis=-1)
+    first_output_model = Model(inputs=CNN_model.input, outputs=output_mse)
 
-    for i in range(app.N_VARS_OUT):
-        print('Rescale back component ' + str(i))
-        Y_pred[:, i] *= model_config['rms'][i] \
-                            [model_config['ypos_Ret'][str(app.TARGET_YP)]] / \
-                        u_rms
-        Y_test[:, i] *= model_config['rms'][i] \
-                            [model_config['ypos_Ret'][str(app.TARGET_YP)]] / \
-                        u_rms
+    # add extra first dim back to keep the shape
+    Y_pred_next = first_output_model.predict(X_test[i][None, :])
+    if i == 0:
+        Y_pred_total = Y_pred_next
+    else:
+        Y_pred_total = np.concatenate([Y_pred_total, Y_pred_next])
+    z = 0
+
+test_mse = mse_orig - np.mean(Y_pred_total)
+
+
+#
+# print(type(Y_pred))
+# print(np.shape(Y_pred))
+#
+# # Revert back to the flow field
+# if app.SCALE_OUTPUT == True:
+#     u_rms = model_config['rms'][0] \
+#         [model_config['ypos_Ret'][str(app.TARGET_YP)]]
+#
+#     for i in range(app.N_VARS_OUT):
+#         print('Rescale back component ' + str(i))
+#         Y_pred[:, i] *= model_config['rms'][i] \
+#                             [model_config['ypos_Ret'][str(app.TARGET_YP)]] / \
+#                         u_rms
+#         Y_test[:, i] *= model_config['rms'][i] \
+#                             [model_config['ypos_Ret'][str(app.TARGET_YP)]] / \
+#                         u_rms
 
 # if pred_fluct == True:
 #     for i in range(app.N_VARS_OUT):
